5fe2cf7fdccbbd848c5ea46bf3cef60fb984d5a8
Matt Brennan <matt@153.io>
2017-02-25 18:13:39 +0000

    do an immutable, with nested contexts and stuff

--- src/index.js
+++ src/index.js
@@ -1,38 +1,67 @@
-import get from 'lodash.get';
+import im from 'immutable';
 
 const state = {
 	initialState: ['foo'],
+	initialContext: {
+		foo: {
+			jant: 35
+		}
+	},
 
 	foo: {
-		bar(state, message) {
-			console.log('foo.bar', {state, message});
-			this.transition('baz', {feld: 16});
+		bar(context, message) {
+			console.log('foo.bar', {context, message});
+			this.popState();
+			this.pushState('baz', {feld: 16});
 		}
 	},
 
 	baz: {
-		quux(state, message) {
-			console.log('baz.quux', {state, message});
+		bar(context, message) {
+			console.log('baz.bar', {context, message});
+		},
+
+		quux(context, message) {
+			console.log('baz.quux', {context, message});
+			this.pushState('frob', {dift: 23})
+		},
+
+		frob: {
+			quint(context, message) {
+				console.log('baz.frob.quint', {context, message});
+				this.popState();
+			}
 		}
 	}
-}
+};
 
 class StateMachine {
 	constructor(stateMap) {
-		this.stateMap = stateMap;
-		this.state = stateMap.initialState || [];
-		this.args = {};
+		this.stateMap = im.fromJS(stateMap);
+		this.state = im.List(stateMap.initialState || []);
+		this.context = im.Map(stateMap.initialContext || {});
 	}
 
-	transition(state, args) {
-		this.state = state;
-		this.args = args;
+	popState() {
+		const poppedState = this.state.last();
+		this.state = this.state.pop();
+		this.context = this.context.delete(poppedState);
+	}
+
+	pushState(state, context) {
+		this.state = this.state.push(state);
+		this.context = this.context.set(state, im.Map(context));
 	}
 
 	message(name, message) {
-		const receivers = get(this.stateMap, this.state);
-		if(receivers[name]) {
-			receivers[name].call(this, this.args, message);
+		const receivers = this.stateMap.getIn(this.state);
+		if(receivers.has(name)) {
+			const context = this.state.reduce(
+				(context, key) => context.merge(this.context.get(key)),
+				im.Map()
+			);
+
+			receivers.get(name).call(this, context.toJS(), message);
 		} else {
 			throw new ReferenceError(`Invalid message ${name} for state ${this.state}`);
 		}
@@ -42,6 +71,7 @@ class StateMachine {
 const s = new StateMachine(state);
 
 s.message('bar', {foo: 5});
-s.transition('foo', {bar: 6});
 s.message('bar', {foo: 7});
 s.message('quux', {foo: 7});
+s.message('quint', {bar: 10})
+s.message('bar', {foo: 7});
